35cc68e9773a392be859776ac6c4f910fff4dde0
LuisBH98 <145933359+XLuisDevX@users.noreply.github.com>
2025-01-07 15:30:24 +0100

    countdown timer

--- scenes/WaveManager.gd
+++ scenes/WaveManager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var enemySpawner: Node
 @export var UI_roundInfo: Control
+@export var UI_countdown: Label
 
 signal wave_started(wave_number)
 signal wave_completed(wave_number)
@@ -14,31 +15,50 @@ var waves = [
 ]
 
 var enemies_defeated = 0
+var time_left = 6
 
 @onready var enemy_spawner = $"../EnemySpawner"
 @onready var wave_timer = $WaveTimer
 
 func _ready():
+	UI_countdown.text = str(time_left)
+	wave_timer.wait_time = 1
 	enemySpawner.connect("enemy_dies", Callable(self, "on_enemy_defeated"))
-	
 	wave_timer.connect("timeout", Callable(self, "start_next_wave"))
+	
+	if UI_countdown:
+		print("Nodo asignado desde el editor:", UI_countdown.name)
+	else:
+		print("El nodo asignado está vacío o no está en el árbol.")
+	
+	wave_timer.start()
 	start_game()
 
 func start_game():
 	start_next_wave()
 
 func start_next_wave():
-	wave_timer.stop()
-	UI_roundInfo.increment_round_count()
-	if current_wave < waves.size():
-		current_wave += 1
-		GlobalScene.set_wave_index(current_wave)
-		var wave_data = waves[current_wave - 1]
-		enemy_spawner.start_wave(wave_data["enemy_count"], wave_data["spawn_interval"])
-		enemies_defeated = 0
-		emit_signal("wave_started", current_wave)
+	time_left -= 1
+	if time_left <= 5 and time_left > 0:
+		UI_countdown.visible = true
+		UI_countdown.text = str(time_left)
 	else:
-		print("All waves completed!")
+		UI_countdown.visible = false
+	var tween = create_tween()
+	UI_countdown.modulate = Color(1.0, 0.8, 0.5)
+	UI_countdown.get_child(0).play("mystic_pulse")
+	if time_left <= 0:
+		wave_timer.stop()
+		UI_roundInfo.increment_round_count()
+		if current_wave < waves.size():
+			current_wave += 1
+			GlobalScene.set_wave_index(current_wave)
+			var wave_data = waves[current_wave - 1]
+			enemy_spawner.start_wave(wave_data["enemy_count"], wave_data["spawn_interval"])
+			enemies_defeated = 0
+			emit_signal("wave_started", current_wave)
+		else:
+			print("All waves completed!")
 
 func on_enemy_defeated():
 	enemies_defeated += 1
