552ad6d930c9c2c7cdaab9dece82815bc7168eb4
LuisBH98 <145933359+XLuisDevX@users.noreply.github.com>
2025-01-09 15:36:06 +0100

    start pause menu functionality

--- scenes/UI_Settings.gd
+++ scenes/UI_Settings.gd
@@ -2,11 +2,15 @@ extends Control
 
 @onready var idle_settings = preload("res://assets/ui/Icons/Regular_02.png")
 @onready var pressed_settings = preload("res://assets/ui/Icons/Pressed_02.png")
+@export var UI_Settings_menu: Control
 
+var is_opened = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	if UI_Settings_menu:
+		print("Nodo asignado desde el editor "+UI_Settings_menu.name)
+	else:
+		print("No se ha encontrado el nodo")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,6 +19,8 @@ func _process(delta):
 
 func _on_texture_button_button_up():
 	$TextureButton/TextureRect.texture = idle_settings
+	is_opened = !is_opened
+	UI_Settings_menu.visible = is_opened
 
 
 func _on_texture_button_button_down():
