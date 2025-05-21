8fa167043adec4709dfbe3edbd31833805a6d294
LuisBH98 <145933359+XLuisDevX@users.noreply.github.com>
2025-05-21 21:44:43 +0200

    feat: [develop] update game volume while slider is dragging

--- scenes/UI/VolumeSlider.gd
+++ scenes/UI/VolumeSlider.gd
@@ -12,9 +12,8 @@ func _process(delta):
 	pass
 
 
-func _on_drag_ended(value_changed):
-	GlobalScene.button_pressed.emit()
+func _on_value_changed(value):
 	if value_changed:
-		var current_volume = max(value, 0.01)
+		var current_volume = maxf(value, 0.01)
 		var new_volume = linear_to_db(current_volume / 100)
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), new_volume )
