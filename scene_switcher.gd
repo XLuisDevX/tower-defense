9c6843653e1b2319d2902e68d145b4dd6e4cab66
LuisBH98 <145933359+XLuisDevX@users.noreply.github.com>
2025-09-10 19:45:47 +0200

    fix: [develop] scene switcher

--- scene_switcher.gd
+++ scene_switcher.gd
@@ -9,7 +9,8 @@ func switch_scene(res_path):
 	call_deferred("_deferred_switch_scene", res_path)
 	
 func _deferred_switch_scene(res_path):
-	current_scene.free()
+	if current_scene != null: current_scene.free()
+	#current_scene.free()
 	var s = load(res_path)
 	current_scene = s.instantiate()
 	get_tree().root.add_child(current_scene)
